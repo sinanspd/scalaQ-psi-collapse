817a2593114f26006af01c3ff88583737dff3ff0
Sinan Pehlivanoglu <sinanspd@Sinans-MacBook-Pro.local>
2023-12-05 01:55:05 -0500

    concurrent version

--- chemical-abstract-machines/build.sbt
+++ chemical-abstract-machines/build.sbt
@@ -3,12 +3,25 @@ import Dependencies._
 ThisBuild / scalaVersion     := "2.12.10"
 ThisBuild / version          := "0.1.0-SNAPSHOT"
 ThisBuild / organization     := "app.vizion"
-
+javaOptions += "-Xmx32G"
 lazy val root = (project in file("."))
   .settings(
     name := "scalaQ-psi-collapse",
     libraryDependencies ++= Seq(
       Libraries.logback,
-      "io.chymyst" %% "chymyst-core" % "0.2.0"
+      "io.chymyst" %% "chymyst-core" % "0.2.0",
+      "javax.xml.bind" % "jaxb-api" % "2.3.1",
+      "org.scalanlp" %% "breeze" % "2.1.0",
+      "org.typelevel" %% "spire" % "0.17.0", 
+      "co.fs2" %% "fs2-core" % "3.9.3",
+      "co.fs2" %% "fs2-io" % "3.9.3",
+      "org.typelevel" %% "cats-effect" % "3.5.2"
+    ),
+    javaOptions ++= Seq(
+      
+    // -J params will be added as jvm parameters
+    "-J-Xmx32g",
+     "-Xmx32g"
     )
   )
+  
--- chemical-abstract-machines/src/main/scala/Main.scala
+++ chemical-abstract-machines/src/main/scala/Main.scala
@@ -1,84 +1,173 @@
-package example 
+package example2 
 
 import io.chymyst.jc._
 import org.slf4j.LoggerFactory
 import scala.util.Random
+import scala.math._
+import spire.math._
+import spire.implicits._ 
+import fs2._
+import cats.effect.IO
+import scala.util.control.NoStackTrace
+import cats.Eval
+
+import cats.effect.unsafe.implicits.global
 
 trait Gate 
 case object X extends Gate 
 case class H(ctrl: Int) extends Gate 
 case class CX(ctrl: Int, target: Int) extends Gate
-case object NullGate extends Gate
-case object Measure extends Gate
+case class U(target: Int, power:Int) extends Gate
+case class Swap(q1: Int, q2: Int) extends Gate
+case class Measure(q: Int) extends Gate
 
+final case class InvalidCircuitError(m: String) extends NoStackTrace
 final case class Circuit(remainingGates: List[Gate]) 
-final case class QVec(prop: Double, v: Vector[Boolean])
+final case class QVec(prop: Complex[Double], v: Vector[Boolean], name: String ="")
 
-object Main extends App {
+sealed trait Sampler{
+    def sample() : Vector[Boolean]
+}
 
-    val hScale : Double = 1.0 / Math.sqrt(2.0)
-    val r = m[QVec]
-    val f = m[QVec]
-    val d = m[Unit]
+class FakeSimonSampler(c: Circuit) extends Sampler {
+        val s = getRandomStateSpace()
+
+        private def getRandomStateSpace() = {
+            val numberOfH = c.remainingGates.collect { case a : H => a }.length
+            val s = gen(2, List()).filter(l => l.forall(_ == l.head))
+            s(new Random().nextInt(s.length))
+        }
+
+        private def gen(n: Int, acc: List[Vector[Boolean]]) : List[Vector[Boolean]] = {
+            if (n == 0) {
+                acc
+            } else {
+                if (acc.length == 0) {
+                gen(n - 1, List(Vector(false), Vector(true)))
+                } else {
+                gen(n - 1, (for (i <- acc) yield i :+ false) ++ (for (j <- acc) yield j :+ true))
+                }
+            }
+        }
+
+        def sample() = s
+}
+
+object Main2 extends App {
+
+    val correctAnswers = List() // Fill this out before running 
+    val concurrentInstances = 2 // Fill this out before running 
+    
     val logger = LoggerFactory.getLogger("example.Main")
+    val hScale : Double = 1.0 / Math.sqrt(2.0)
     val decider = new Random()
 
+    val r = m[QVec]
+
+    implicit class CircuitDeferral(c: Circuit){ 
+        def deferMeasurements = {
+            val rearrangedGates = c.remainingGates.foldLeft(List[Gate]())((a, b) => b match{
+                case b : DeferredMeasurement if(a.isEmpty) => 
+                    throw InvalidCircuitError("A DeferredMeasurement gate shouldn't appear at the beginning of a circuit")
+                case b : DeferredMeasurement => {
+                    val prevGate = a.takeRight(1).head
+                    prevGate match{
+                        case g: DeferredMeasurement => a.take(a.size - 1) :+ MultiDeferredMeasure(List(g.q, b.q))
+                        case _ => a :+ b
+                    }
+                }
+                case b : Measure => a :+ b
+                case _ => 
+                    if(a.isEmpty){
+                        a :+ b 
+                    }else{
+                        val prevGate = a.takeRight(1).head
+                         prevGate match{
+                            case DeferredMeasurement(_) | MultiDeferredMeasure(_) => 
+                                a.take(a.size - 1) ++ List(b, prevGate)
+                            case _ => a :+ b
+                        }
+                    }
+            })
+
+            new Circuit(rearrangedGates)
+        }
+    }
+
+    object Circuit{
+        def apply(remainingGates: List[Gate]) = new Circuit(remainingGates).deferMeasurements
+    }
+
+    case class MultiDeferredMeasure(q: List[Int]) extends Gate 
+    case class DeferredMeasurement(q: Int) extends Gate 
+
     site (
-        go { case r(a) + r(b) ⇒ 
+        go { case r(a) + r(b) ⇒ {
                 logger.debug(s"Possible Reaction between $a and $b"); 
-                if(a.v.sameElements(b.v)){
+                if(a.v.sameElements(b.v)){ 
                     logger.debug("Molecules compatible. Reaction starting")
                     val newQ = QVec(a.prop + b.prop, a.v)
-                    if(newQ.prop.abs > .4){
-                        logger.info("I am tired. Time to go: " + newQ)
-                        f(newQ)
+                    if(newQ.prop.real.abs >= 0.6){
+                        logger.info(s"Result $newQ")
+                    }else if(newQ.prop.real.abs == 0d){
+                        logger.debug("Destrictive Interference")
                     }else{
+                        logger.debug(s"Reaction finished, releasesing $newQ into the solution")
                         r(newQ)
                     }
                 }else{
                     logger.debug("Incompatible molecules. Returning molecules to the pool")
                     r(a) + r(b)
                 }
-        },
-
-        go {case f(a) + f(b) ⇒ 
-                if(decider.nextInt(1) > 0){
-                    logger.info("Eliminated " + b + " , Remains: " + a)
-                    f(a)
-                }else{
-                    logger.info("Eliminated " + a + ", Remains: " + b)
-                    f(b)
-                }
-
+            }
         }
     )
 
-    def build(c: Circuit, v: QVec) : Unit = {
+    def build(c: Circuit, v: QVec)(implicit sampler: Sampler) : Unit = {
         c.remainingGates match{ 
             case Nil => 
-                logger.debug("Releasing " + v)
-                r(v)
+                val midresult = sampler.sample()
+                if(v.v.takeRight(v.v.length / 2).sameElements(midresult)){
+                    val newMolecule = QVec(v.prop, v.v.take(v.v.length / 2))
+                    logger.debug("Releasing " + newMolecule)
+                    r(newMolecule)
+                }
             case x :: xs => 
                 x match {
                     case H(t) => 
                         val sign = if (v.v(t)) -1 else 1
-                        val x1 = QVec(sign * hScale * v.prop, v.v)
-                        val x2 =  QVec(hScale * v.prop, v.v.updated(t, !v.v(t)))
-                        build(Circuit(xs), x1)
-                        build(Circuit(xs), x2)
+                        val h1 = QVec(sign * hScale * v.prop, v.v)
+                        val h2 =  QVec(hScale * v.prop, v.v.updated(t, !v.v(t)))
+                        build(Circuit(xs), h1)
+                        build(Circuit(xs), h2)
                     case CX(c, t) => 
                         if(v.v(c)){
                             build(Circuit(xs), QVec(v.prop, v.v.updated(t, !(v.v(t)))))
                         }else{
                             build(Circuit(xs), QVec(v.prop, v.v))
                         }
+                    case Swap(q1, q2) => 
+                         val source = v.v(q1)
+                         val target = v.v(q2)
+                         val firstOverwrite = v.v.updated(q2, source)
+                         val second = firstOverwrite.updated(q1, target)
+                         build(Circuit(xs), QVec(v.prop, second))
+                    case Measure(q) => ??? 
                 }
         }
     }
 
-    build(Circuit(List(H(0), H(1), CX(0, 2), CX(0, 3), CX(1, 2), CX(1, 3), H(0), H(1))),
-         QVec(1d, Vector(false, false, false, false)))
+    //shor filter based on 0001 
+    // a^x mod n
+
+    val fourbitsimon = Circuit(List(H(0), H(1), CX(0, 2), CX(0, 3), CX(1, 2), CX(1, 3), H(0), H(1)))
+    val excs = QVec(1d, Vector(false, false, false, false))
+
+    implicit val sampler = new FakeSimonSampler(fourbitsimon)
+
+    Stream.eval(IO{build(fourbitsimon, excs)}).repeatN(concurrentInstances).compile.toVector.unsafeRunSync()
 
     //shores 
     //grovers 
+
 }
